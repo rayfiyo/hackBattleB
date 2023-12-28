df0fcc5687d2fb830c349d9b4657bea641c495c9
rayfiyo <rayfiyorex@gmail.com>
2023-12-28 15:35:47 +0900

    backup

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -1,19 +1,10 @@
 package handler
 
 import (
-	"api/model"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-func NewTitle() *TitleHandler {
-	return &TitleHandler{TitleInfo: &model.TitleInfo{}}
-}
-
-type TitleHandler struct {
-	*model.TitleInfo
-}
 
 func Clear(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
@@ -21,18 +12,11 @@ func Clear(c *gin.Context) {
 	})
 }
 
+/*
 func GetMode(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"ModeID": 0,
 	})
-}
-
-func (h *TitleHandler) GetTitle(c *gin.Context) {
-	titlesArray := make([]gin.H, len(h.Titles))
-	for i, t := range h.Titles {
-		titlesArray[i] = gin.H{"title": t}
-	}
-	c.JSON(http.StatusOK, titlesArray)
 }
 
 func SetMode(c *gin.Context) {
@@ -40,11 +24,4 @@ func SetMode(c *gin.Context) {
 		"Status": "OK",
 	})
 }
-
-func (h *TitleHandler) SendTitle(c *gin.Context) {
-	title := c.Query("title")
-	h.Titles = append(h.Titles, title)
-	c.JSON(http.StatusOK, gin.H{
-		"Status": "OK",
-	})
-}
+*/
